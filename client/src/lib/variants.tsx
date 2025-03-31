8f62b00a45280561b855057678783b5c541ffbcc
nebbilies <hirohamada17092005@gmail.com>
2025-03-31 14:54:28 +0700

    chore: update variants

--- client/src/lib/variants.tsx
+++ client/src/lib/variants.tsx
@@ -1,7 +1,7 @@
 const variants = {
-    hidden: { opacity: 0, x: 0, y: 100 },
+    hidden: { opacity: 0, x: 0, y: 60 },
     enter: { opacity: 1, x: 0, y: 0 },
-    exit: { opacity: 0, x: 0, y: -100 },
+    exit: { opacity: 0, x: 0, y: -60 },
 };
 
 export { variants }
