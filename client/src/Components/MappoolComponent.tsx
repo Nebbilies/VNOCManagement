3fd3e6f45b3c9584b6b532d37efca52ecd6566b6
nebbilies <hirohamada17092005@gmail.com>
2025-05-10 13:57:23 +0700

    feat: "add beatmap" search

--- client/src/Components/MappoolComponent.tsx
+++ client/src/Components/MappoolComponent.tsx
@@ -2,27 +2,32 @@ import MappoolContent from "./MappoolContent.tsx";
 import {useEffect, useState, createContext, useContext} from "react";
 import {motion, AnimatePresence} from "motion/react";
 import {MappoolContext} from "../context/MappoolContext.tsx";
+import { Search } from "lucide-react";
 
 const mapsId: RawPoolData = {
     "NM": [
         {
-            "id": 2258243,
+            "id": 1363001,
             "idx": 1,
         },
         {
-            "id": 2223850,
+            "id": 4977277,
             "idx": 2,
         },
     ],
     "HD": [
         {
-            "id": 2258243,
+            "id": 3621640,
             "idx": 1,
         },
+        {
+            "id": 1972244,
+            "idx": 2,
+        }
     ],
     "HR": [
         {
-            "id": 2258243,
+            "id": 1363001,
             "idx": 1,
         },
     ],
@@ -122,9 +127,46 @@ const AddBeatmapsButton = () => {
     const [beatmapId, setBeatmapId] = useState('');
     const [mappoolName, setMappoolName] = useState('');
     const [slotName, setSlotName] = useState('');
-
+    const [beatmapSearchId, setBeatmapSearchId] = useState('');
+    const [beatmapSearchData, setBeatmapSearchData] = useState({
+        returnCode: -1,
+        id: -1,
+        artist: '',
+        title: '',
+        difficulty: '',
+    })
     const openModal = () => setIsModalOpen(true);
     const closeModal = () => setIsModalOpen(false);
+
+    useEffect(() => {
+        async function fetchBeatmapData() {
+            const response = await fetch(`https://tryz.vercel.app/api/b/${beatmapSearchId}`);
+            const json = await response.json();
+            if ("error" in json) {
+                setBeatmapSearchData({
+                    returnCode: json.error.status,
+                    id: -1,
+                    artist: '',
+                    title: '',
+                    difficulty: '',
+                });
+            } else {
+                const jsonData: MapResponse = json;
+                const beatmapData = jsonData.beatmaps.find((map) => map.id === parseInt(beatmapSearchId));
+                setBeatmapSearchData({
+                    returnCode: 200,
+                    id: json.id,
+                    artist: json.artist,
+                    title: json.title,
+                    difficulty: beatmapData!.version,
+                });
+            }
+            console.log(beatmapSearchData.returnCode)
+        }
+        if (beatmapSearchId) {
+            fetchBeatmapData()
+        }
+    }, [beatmapSearchId]);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -161,9 +203,9 @@ const AddBeatmapsButton = () => {
         <>
             <button
                 onClick={openModal}
-                className="bg-violet-500 text-2xl w-1/4 hover:bg-violet-700 text-white font-bold py-2 px-4 mx-4 cursor-pointer rounded rounded-2xl transition-colors duration-300"
+                className="bg-green-500 text-2xl w-1/4 hover:bg-green-700 text-white font-bold py-2 px-4 mx-4 cursor-pointer rounded rounded-2xl transition-colors duration-300"
             >
-                Add Beatmaps
+                +
             </button>
 
             <AnimatePresence>
@@ -174,7 +216,7 @@ const AddBeatmapsButton = () => {
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 0.6 }}
                             exit={{ opacity: 0 }}
-                            className="fixed inset-0 bg-gray-600 blur-xl z-40"
+                            className="fixed inset-0 bg-gray-700 blur-l z-40"
                             onClick={closeModal}
                         />
 
@@ -184,28 +226,79 @@ const AddBeatmapsButton = () => {
                             animate={{ opacity: 1, scale: 1, y: 0 }}
                             exit={{ opacity: 0, scale: 0.9, y: 20 }}
                             transition={{ type: "spring", damping: 25, stiffness: 300 }}
-                            className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-lg shadow-xl p-6 z-50 w-96"
+                            className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#23263a] rounded-lg shadow-2xl p-6 z-50 w-96"
                         >
-                            <h2 className="text-xl font-semibold mb-4 text-gray-500">Add Beatmap</h2>
+                            <h2 className="text-xl font-semibold mb-4 text-white">Add Beatmap</h2>
 
                             <form onSubmit={handleSubmit}>
                                 <div className="mb-4">
-                                    <label htmlFor="beatmapId" className="block text-sm font-medium text-gray-700 mb-1">
+                                    <label htmlFor="beatmapId" className="block text-sm font-medium text-white mb-1">
                                         Beatmap ID
                                     </label>
-                                    <input
-                                        type="text"
-                                        id="beatmapId"
-                                        value={beatmapId}
-                                        onChange={(e) => setBeatmapId(e.target.value)}
-                                        className="w-full p-2 border border-gray-300 font-normal text-xl rounded focus:ring-violet-500 focus:border-violet-500 text-black"
-                                        required
-                                    />
+                                    <div className={'flex'}>
+                                        <input
+                                            type="text"
+                                            id="beatmapId"
+                                            value={beatmapId}
+                                            onChange={(e) => setBeatmapId(e.target.value)}
+                                            className="w-full p-2 border border-gray-300 font-normal text-xl rounded focus:ring-violet-500 focus:border-violet-500 text-white"
+                                            required
+                                        />
+                                        <button className={'bg-gray-900/20 text-white text-2xl rounded w-1/6 hover:bg-gray-700 transition-colors flex justify-center items-center ml-2 border-white border-1'}
+                                        onClick = {() => setBeatmapSearchId(beatmapId)}
+                                        >
+                                            <Search className={'w-8 h-8'}/>
+                                        </button>
+                                    </div>
                                 </div>
+                                {/* Beatmap Search Result */
+                                    //--------------------
+                                    //--------------------
+                                    // REMEMBER TO CHECK FOR ADDITIONAL CONDITIONS WHEN BACKEND FINISHES
+                                    // 1. MAKE MAPPOOL NAME A DROP DOWN OF ROUNDS
+                                    // 2. CHECK IF SLOT NAME EXISTS IN MAPPOOL
+                                    //---------------------
+                                    //--------------------
+                                }
+                                {beatmapSearchData.returnCode === 404 ? (
+                                    <div className="text-red-500 text-sm">
+                                        Beatmap not found
+                                    </div>
+                                ) : null}
+                                {beatmapSearchData.returnCode === 200 ? (
+                                    <div
+                                        className={`mappool-content-map-item flex w-full rounded-3xl text-black h-20 font-bold items-center bg-center bg-cover bg-white/70 sm:bg-white/90 border-2 border-white `}
+                                        style={{
+                                            backgroundImage: `url('https://assets.ppy.sh/beatmaps/${beatmapSearchData.id}/covers/raw.jpg')`,
+                                            backgroundBlendMode: 'overlay'
+                                        }}>
+                                        <img className={`object-cover h-full w-auto sm:flex hidden rounded-r-3xl rounded-l-3xl`}
+                                             src={`https://assets.ppy.sh/beatmaps/${beatmapSearchData.id}/covers/raw.jpg`}
+                                             alt={'bg'}/>
+                                        <div
+                                            className={`mappool-content-map-item-info flex flex-col w-7/12 h-full px-3 justify-start text-start overflow-hidden`}
+                                        >
+                                            <div
+                                                className={'mappool-content-map-item-info-name text-lg font-bold truncate inline'}>
+                                                {beatmapSearchData.title}
+                                            </div>
+                                            <div
+                                                className={`mappool-content-map-item-info-artist  w-full text-xs font-normal truncate`}>
+                                                {beatmapSearchData.artist}
+                                            </div>
+                                            <div
+                                                className={'mappool-content-map-item-info-mapper flex w-full text-xs font-bold mt-3 truncate'}>
+                                                {beatmapSearchData.difficulty}
+                                            </div>
+                                        </div>
+                                    </div>
+                                ) : null}
 
-                                <div className="flex gap-4 mb-6">
+                                {/* Mappool Name and Slot Name */}
+                                <div className="flex gap-4 mt-4 mb-6">
                                     <div className="w-1/2">
-                                        <label htmlFor="mappoolName" className="block text-sm font-medium text-gray-700 mb-1">
+                                        <label htmlFor="mappoolName"
+                                               className="block text-sm font-medium text-white mb-1">
                                             Mappool Name
                                         </label>
                                         <input
@@ -213,13 +306,13 @@ const AddBeatmapsButton = () => {
                                             id="mappoolName"
                                             value={mappoolName}
                                             onChange={(e) => setMappoolName(e.target.value)}
-                                            className="w-full p-2 border border-gray-300 font-normal text-xl rounded focus:ring-violet-500 focus:border-violet-500 text-black"
+                                            className="w-full p-2 border border-gray-300 font-normal text-xl rounded focus:ring-violet-500 focus:border-violet-500 text-white"
                                             required
                                         />
                                     </div>
 
                                     <div className="w-1/2">
-                                        <label htmlFor="slotName" className="block text-sm font-medium text-gray-700 mb-1">
+                                        <label htmlFor="slotName" className="block text-sm font-medium text-white mb-1">
                                             Slot Name
                                         </label>
                                         <input
@@ -227,7 +320,7 @@ const AddBeatmapsButton = () => {
                                             id="slotName"
                                             value={slotName}
                                             onChange={(e) => setSlotName(e.target.value)}
-                                            className="w-full p-2 border border-gray-300 font-normal text-xl rounded focus:ring-violet-500 focus:border-violet-500 text-black"
+                                            className="w-full p-2 border border-gray-300 font-normal text-xl rounded focus:ring-violet-500 focus:border-violet-500 text-white"
                                             required
                                         />
                                     </div>
@@ -237,13 +330,13 @@ const AddBeatmapsButton = () => {
                                     <button
                                         type="button"
                                         onClick={closeModal}
-                                        className="text-gray-700 text-2xl bg-gray-200 w-1/2 h-full rounded hover:bg-gray-300 transition-colors"
+                                        className="text-gray-700 text-2xl bg-gray-200 w-1/2 h-full rounded hover:bg-gray-300 transition-colors cursor-pointer"
                                     >
                                         Cancel
                                     </button>
                                     <button
                                         type="submit"
-                                        className="bg-blue-500 text-white text-2xl rounded w-1/2 hover:bg-blue-600 transition-colors"
+                                        className={`bg-blue-500 text-white text-2xl rounded w-1/2 hover:bg-blue-600 transition-colors ${beatmapSearchData.returnCode === 200 ? 'cursor-pointer' : "cursor-not-allowed opacity-50"} `}
                                     >
                                         Confirm
                                     </button>
