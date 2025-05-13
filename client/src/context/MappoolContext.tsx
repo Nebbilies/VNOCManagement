e6258cdc5b14ad417cc6a6af26c076c115153931
nebbilies <hirohamada17092005@gmail.com>
2025-05-13 10:55:01 +0700

    chore: updated context

--- client/src/context/MappoolContext.tsx
+++ client/src/context/MappoolContext.tsx
@@ -1,3 +1,4 @@
 import {createContext} from "react";
+import {PoolData} from "../Components/MappoolContent.tsx";
 
-export const MappoolContext = createContext({})
+export const MappoolContext = createContext<PoolData>({DT: [], HD: [], HR: [], NM: [], TB: []})
