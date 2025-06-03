12a3051182c00fb38ec8eab5f2d6016daa675932
meofc <23521787@ms.uit.edu.vn>
2025-06-03 22:48:56 +0700

    feat: Match feature added

--- server/routes/MatchRouter.js
+++ server/routes/MatchRouter.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const MatchController = require("../controllers/MatchController");
+const checkRole = require("../middlewares/checkRole");
 
+// Public route
 router.get("/", MatchController.getAllMatches);
+
+// Protected routes (admin or referee)
+router.post("/add", checkRole, MatchController.addMatch);
+router.put("/edit", checkRole, MatchController.editMatch);
+router.delete("/:id", checkRole, MatchController.deleteMatch);
+router.post("/claim", checkRole, MatchController.claimMatch);
 
 module.exports = router;
