bc4e102ba8703911c1d3ad8023e98b1c4b3187a8
meofc <23521787@ms.uit.edu.vn>
2025-06-05 13:01:05 +0700

    feat: add delete round

--- server/controllers/RoundController.js
+++ server/controllers/RoundController.js
@@ -52,8 +52,35 @@ module.exports = {
             return res.json({ message: "Round edited"});
 
         } catch (err) {
-            console.error("[addRound] Error occurred:", err.message);
+            console.error("[editRound] Error occurred:", err.message);
             return res.status(500).json({ error: "Failed to edit round" });
+        }
+    },
+
+    async deleteRound(req, res) {
+        if (req.user.role !== "ADMIN") {
+            return res.status(403).json({ error: "Forbidden" });
+        }
+
+        try {
+            const {Acronym} = req.body;
+
+            const [check1] = await pool.query(`SELECT * FROM map WHERE Round = ?`, Acronym);
+            const [check2] = await pool.query(`SELECT * FROM matches WHERE Round = ?`, Acronym);
+
+            if (check1.length !== 0 || check2.length !== 0) {
+                return res.status(409).json({ error: "Round maps and/or round matches exist" });
+            }
+
+            const [rows] = await pool.query(`DELETE FROM rounds WHERE Acronym = ?`, Acronym);
+            if (rows.affectedRows === 0) {
+                return res.status(404).json({ error: "Round not found" });
+            }
+
+            return res.json({ message: "Round deleted" });
+        } catch (err) {
+            console.error("[deleteRound] Error occurred:", err.message);
+            return res.status(500).json({ error: "Failed to delete round" });
         }
     }
 }
--- server/routes/RoundRouter.js
+++ server/routes/RoundRouter.js
@@ -7,5 +7,6 @@ const checkRole = require("../middlewares/checkRole");
 router.get("/", RoundController.getRound);
 router.post("/add", checkRole, RoundController.addRound);
 router.post("/edit", checkRole, RoundController.editRound);
+router.delete("/delete", checkRole, RoundController.deleteRound);
 
 module.exports = router;
